0213160d9135fd57629dc35ac6465ecf4a74db12
Yan Chen <yanche.belongs@gmail.com>
2017-06-18 15:55:44 -0700

    update & use ts

---
+++ index.ts
@@ -0,0 +1,38 @@
+﻿
+export default class Hub<T> {
+    private _fn: () => PromiseLike<T>;
+    private _timeout: number;
+    private _lastRunTs: number;
+    private _activePromise: Promise<T>;
+
+    public timeout(): boolean {
+        return Boolean(this._timeout > 0 && this._lastRunTs && ((this._timeout + this._lastRunTs) <= new Date().getTime()));
+    }
+
+    public get(): Promise<T> {
+        if (this._activePromise && !this.timeout())
+            return this._activePromise;
+        else {
+            this._lastRunTs = new Date().getTime();
+            return this._activePromise = new Promise<T>((res, rej) => {
+                try {
+                    this._fn().then(res, (err: Error) => {
+                        rej(err);
+                        this._activePromise = null;
+                    });
+                }
+                catch (err) {
+                    // error in this._fn()
+                    rej(err);
+                    this._activePromise = null;
+                }
+            });
+        }
+    }
+
+    constructor(fn: () => PromiseLike<T>, timeout?: number) {
+        this._fn = fn;
+        this._timeout = timeout; //in ms
+        this._activePromise = this._lastRunTs = null;
+    }
+};
---
+++ sample.ts
@@ -0,0 +1,36 @@
+﻿
+import Hub from "./index.js";
+
+function asyncFn() {
+	console.log('in function');
+	return delay(2000).then(() => 200);
+};
+
+const hub = new Hub<number>(asyncFn, 5000);
+
+Promise.all([hub.get(), hub.get()])
+	.then(data => {
+		console.log('both complete');
+		console.info(data);
+	})
+	.then(function () {
+		//4.5s later, hub cache still work
+
+		return Promise.all([
+			delay(1000)
+				.then(() => {
+					console.log("you will get result immediately, it's from cache");
+					return hub.get().then(d => console.log(d));
+				}),
+			delay(5000)
+				.then(() => {
+					console.log("you will not get result immediately, because cache expired");
+					return hub.get().then(d => console.log(d));
+				})
+		]);
+	})
+	.catch((err: Error) => console.error(err.stack));
+
+function delay(ms: number): Promise<void> {
+	return new Promise<void>(res => setTimeout(res, ms));
+}
